96f7b60418990d54eb2109596fff339142c79190
benyiannabella@yahoo.com <benyiannabella@yahoo.com>
2021-12-08 21:25:12 +0200

    first commit

---
+++ src/main/java/test/spring5/spring_dep_injection/SpringDepInjectionApplication.java
@@ -0,0 +1,13 @@
+package test.spring5.spring_dep_injection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDepInjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDepInjectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/test/spring5/spring_dep_injection/SpringDepInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package test.spring5.spring_dep_injection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDepInjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
